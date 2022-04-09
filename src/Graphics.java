cfa10d8ab4782c70ab36909a381394af800f4d52
Khanh <thnkhanh19@clc.fitus.edu.vn>
2022-04-09 12:17:24 +0700

    Initial commit

---
+++ src/Graphics.java
@@ -0,0 +1,75 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Graphics 
+extends JPanel
+implements ActionListener{
+	private Timer t = new Timer(100, this);	
+	public String state;
+	
+	private Snake s;
+	private Food f;
+	private Game game;
+	
+	private int w = Game.width;
+	private int h = Game.height;
+	private int d = Game.dimension;
+	
+	public Graphics(Game g)
+	{
+		t.start();
+		state = "START";
+		game = g;
+		s = g.getPlayer();
+		f = g.getFood();
+		
+		this.addKeyListener(g);
+		this.setFocusable(true);
+		this.setFocusTraversalKeysEnabled(false);
+	}
+	
+	public void paintComponent(java.awt.Graphics g)
+	{
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setColor(Color.black);
+		g2d.fillRect(0, 0, w * d + 5, h * d + 5);
+		
+		if(state == "START")
+		{
+			g2d.setColor(Color.white);
+			g2d.drawString("Press Any Key", w/2 * d - 40, h/2 * d - 20);
+		}
+		else if(state == "RUNNING")
+		{
+			g2d.setColor(Color.red);
+			g2d.fillRect(f.getX() * d, f.getY() * d, d, d);
+			
+			g2d.setColor(Color.green);
+			for (Rectangle r : s.getBody())
+			{
+				g2d.fill(r);
+			}
+		}
+		else 
+		{
+			g2d.setColor(Color.white);
+			g2d.drawString("Your Score" + (s.getBody().size() - 3), w/2 * d - 40, h/2 * d - 20);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+		
+		game.update();
+	}
+}
